194696b8e30e7e795b38389a6cfd9b2c17dfa284
TingWei Ou <87459096+ouotingwei@users.noreply.github.com>
2023-07-09 21:13:08 +0800

    Create scikit_learn_svc.py

---
+++ scikit_learn_svc.py
@@ -0,0 +1,20 @@
+import pandas as pd
+url = "https://raw.githubusercontent.com/GrandmaCan/ML/main/Classification/Diabetes_Data.csv"
+data = pd.read_csv(url)
+
+# gendar boy = 1, girl = 0
+data["Gender"] = data["Gender"].map({"男生" : 1, "女生" : 0})
+
+from sklearn.model_selection import train_test_split
+x = data[["Age", "Weight", "BloodSugar", "Gender"]]
+y = data["Diabetes"]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=87)
+x_train = x_train.to_numpy()
+x_test = x_test.to_numpy()
+x_train, x_test
+
+from sklearn.svm import SVC
+svm_model = SVC()
+svm_model.fit(x_train, y_train)
+y_pred = svm_model.predict(x_test)
+(y_pred==y_test).sum() / len(y_test)
